7a8f73aef5d3d6775e912cc5be65186817dca519
faith-choi <two2.apr@gmail.com>
2022-06-04 16:30:02 +0900

    진짜진짜최종

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connect = require("./schemas");
 const router = express.Router();
 const app = express();
-const port = 8080;
+const port = 3000;
 
 connect();
 
